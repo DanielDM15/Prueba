96cce441754a89a9c2d4b49a7ec0ad56a1897117
Daniel14 <aarondadm@gmail.com>
2025-11-26 17:19:30 -0600

    Chivas

--- src/main/java/mx/uaemex/fi/pruebas/demo/HelloController.java
+++ src/main/java/mx/uaemex/fi/pruebas/demo/HelloController.java
@@ -2,13 +2,17 @@ package mx.uaemex.fi.pruebas.demo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML private ImageView chiva;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+        chiva.setVisible(true);
+        welcomeText.setText("Arriba las chivas---");
     }
 }
