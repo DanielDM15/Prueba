d23fe14d9d4094e2e035b4b62925f3929307f482
Daniel14 <aarondadm@gmail.com>
2025-11-26 17:42:42 -0600

    Arriba las chivas

--- src/main/java/mx/uaemex/fi/pruebas/demo/HelloController.java
+++ src/main/java/mx/uaemex/fi/pruebas/demo/HelloController.java
@@ -1,6 +1,7 @@
 package mx.uaemex.fi.pruebas.demo;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
@@ -8,11 +9,12 @@ public class HelloController {
     @FXML
     private Label welcomeText;
     @FXML private ImageView chiva;
-
+    @FXML private Button btnAction;
     @FXML
     protected void onHelloButtonClick() {
 
         chiva.setVisible(true);
         welcomeText.setText("Arriba las chivas---");
+        btnAction.setVisible(false);
     }
 }
